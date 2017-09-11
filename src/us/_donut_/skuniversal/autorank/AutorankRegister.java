632cc34c8a88fc81f96476cf62624bdfd47169ee
Donut <donut42915@gmail.com>
2017-09-10 23:04:42 -0400

    Add files via upload

--- src/us/_donut_/skuniversal/autorank/AutorankRegister.java
+++ src/us/_donut_/skuniversal/autorank/AutorankRegister.java
@@ -14,14 +14,14 @@ public class AutorankRegister {
         if (Bukkit.getServer().getPluginManager().getPlugin("Autorank") != null) {
 
             //Expressions
-            Skript.registerExpression(ExprGlobalTime.class, Number.class, ExpressionType.PROPERTY, "[the] global [Autorank] [play[ ]]time of %player%", "%player%'s global [Autorank] [play[ ]]time");
-            Skript.registerExpression(ExprLocalTime.class, Number.class, ExpressionType.PROPERTY, "[the] local [Autorank] [play[ ]]time of %player%", "%player%'s local [Autorank] [play[ ]]time");
-            Skript.registerExpression(ExprAllRequirements.class, String.class, ExpressionType.PROPERTY, "[the] [Autorank] (requirements|reqs) of %player%", "%player%'s [Autorank] (requirements|reqs)");
-            Skript.registerExpression(ExprCompletedReqs.class, String.class, ExpressionType.PROPERTY, "[the] completed [Autorank] (requirements|reqs) of %player%", "%player%'s completed [Autorank] (requirements|reqs)");
-            Skript.registerExpression(ExprFailedReqs.class, String.class, ExpressionType.PROPERTY, "[the] failed [Autorank] (requirements|reqs) of %player%", "%player%'s failed [Autorank] (requirements|reqs)");
-            Skript.registerExpression(ExprCurrentPath.class, String.class, ExpressionType.PROPERTY, "[the] [(current|active)] [Autorank] path of %player%", "%player%'s [(current|active)] [Autorank] path");
-            Skript.registerExpression(ExprEligiblePaths.class, String.class, ExpressionType.PROPERTY, "[the] eligible [Autorank] paths of %player%", "%player%'s eligible [Autorank] paths");
-            Skript.registerExpression(ExprCompletedPaths.class, String.class, ExpressionType.PROPERTY, "[the] (completed|finished) [Autorank] paths of %player%", "%player%'s (completed|finished) [Autorank] paths");
+            Skript.registerExpression(ExprGlobalTime.class, Number.class, ExpressionType.COMBINED, "[the] global [Autorank] [play[ ]]time of %player%", "%player%'s global [Autorank] [play[ ]]time");
+            Skript.registerExpression(ExprLocalTime.class, Number.class, ExpressionType.COMBINED, "[the] local [Autorank] [play[ ]]time of %player%", "%player%'s local [Autorank] [play[ ]]time");
+            Skript.registerExpression(ExprAllRequirements.class, String.class, ExpressionType.COMBINED, "[the] [Autorank] (requirements|reqs) of %player%", "%player%'s [Autorank] (requirements|reqs)");
+            Skript.registerExpression(ExprCompletedReqs.class, String.class, ExpressionType.COMBINED, "[the] completed [Autorank] (requirements|reqs) of %player%", "%player%'s completed [Autorank] (requirements|reqs)");
+            Skript.registerExpression(ExprFailedReqs.class, String.class, ExpressionType.COMBINED, "[the] failed [Autorank] (requirements|reqs) of %player%", "%player%'s failed [Autorank] (requirements|reqs)");
+            Skript.registerExpression(ExprCurrentPath.class, String.class, ExpressionType.COMBINED, "[the] [(current|active)] [Autorank] path of %player%", "%player%'s [(current|active)] [Autorank] path");
+            Skript.registerExpression(ExprEligiblePaths.class, String.class, ExpressionType.COMBINED, "[the] eligible [Autorank] paths of %player%", "%player%'s eligible [Autorank] paths");
+            Skript.registerExpression(ExprCompletedPaths.class, String.class, ExpressionType.COMBINED, "[the] (completed|finished) [Autorank] paths of %player%", "%player%'s (completed|finished) [Autorank] paths");
 
             //Events
             Skript.registerEvent("Autorank Requirement Completion", SimpleEvent.class, RequirementCompleteEvent.class, "[on] [Autorank] requirement complet(e|ion)");
