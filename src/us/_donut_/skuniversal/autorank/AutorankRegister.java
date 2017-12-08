cc1a17ad5a38f14d19b6860eedef59961c800f1a
Donut <donut42915@gmail.com>
2017-12-08 14:51:02 -0500

    Add files via upload

--- src/us/_donut_/skuniversal/autorank/AutorankRegister.java
+++ src/us/_donut_/skuniversal/autorank/AutorankRegister.java
@@ -2,41 +2,36 @@ package us._donut_.skuniversal.autorank;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.ExpressionType;
-import ch.njol.skript.lang.util.SimpleEvent;
 import ch.njol.skript.registrations.EventValues;
 import ch.njol.skript.util.Getter;
 import me.armar.plugins.autorank.api.events.RequirementCompleteEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import us._donut_.skuniversal.SkUniversal;
+import us._donut_.skuniversal.SkUniversalEvent;
 
 public class AutorankRegister {
+
     public static void registerAutorank() {
-        if (Bukkit.getServer().getPluginManager().getPlugin("Autorank") != null) {
-            SkUniversal.hookedPlugins.add("Autorank");
+        //Expressions
+        Skript.registerExpression(ExprGlobalTime.class, Number.class, ExpressionType.COMBINED, "[the] global [Autorank] [play[ ]]time of %player%", "%player%'s global [Autorank] [play[ ]]time");
+        Skript.registerExpression(ExprLocalTime.class, Number.class, ExpressionType.COMBINED, "[the] local [Autorank] [play[ ]]time of %player%", "%player%'s local [Autorank] [play[ ]]time");
+        Skript.registerExpression(ExprAllRequirements.class, String.class, ExpressionType.COMBINED, "[the] [Autorank] (requirements|reqs) of %player%", "%player%'s [Autorank] (requirements|reqs)");
+        Skript.registerExpression(ExprCompletedReqs.class, String.class, ExpressionType.COMBINED, "[the] completed [Autorank] (requirements|reqs) of %player%", "%player%'s completed [Autorank] (requirements|reqs)");
+        Skript.registerExpression(ExprFailedReqs.class, String.class, ExpressionType.COMBINED, "[the] failed [Autorank] (requirements|reqs) of %player%", "%player%'s failed [Autorank] (requirements|reqs)");
+        Skript.registerExpression(ExprCurrentPath.class, String.class, ExpressionType.COMBINED, "[the] [(current|active)] [Autorank] path of %player%", "%player%'s [(current|active)] [Autorank] path");
+        Skript.registerExpression(ExprEligiblePaths.class, String.class, ExpressionType.COMBINED, "[the] eligible [Autorank] paths of %player%", "%player%'s eligible [Autorank] paths");
+        Skript.registerExpression(ExprCompletedPaths.class, String.class, ExpressionType.COMBINED, "[the] (completed|finished) [Autorank] paths of %player%", "%player%'s (completed|finished) [Autorank] paths");
 
-            //Expressions
-            Skript.registerExpression(ExprGlobalTime.class, Number.class, ExpressionType.COMBINED, "[the] global [Autorank] [play[ ]]time of %player%", "%player%'s global [Autorank] [play[ ]]time");
-            Skript.registerExpression(ExprLocalTime.class, Number.class, ExpressionType.COMBINED, "[the] local [Autorank] [play[ ]]time of %player%", "%player%'s local [Autorank] [play[ ]]time");
-            Skript.registerExpression(ExprAllRequirements.class, String.class, ExpressionType.COMBINED, "[the] [Autorank] (requirements|reqs) of %player%", "%player%'s [Autorank] (requirements|reqs)");
-            Skript.registerExpression(ExprCompletedReqs.class, String.class, ExpressionType.COMBINED, "[the] completed [Autorank] (requirements|reqs) of %player%", "%player%'s completed [Autorank] (requirements|reqs)");
-            Skript.registerExpression(ExprFailedReqs.class, String.class, ExpressionType.COMBINED, "[the] failed [Autorank] (requirements|reqs) of %player%", "%player%'s failed [Autorank] (requirements|reqs)");
-            Skript.registerExpression(ExprCurrentPath.class, String.class, ExpressionType.COMBINED, "[the] [(current|active)] [Autorank] path of %player%", "%player%'s [(current|active)] [Autorank] path");
-            Skript.registerExpression(ExprEligiblePaths.class, String.class, ExpressionType.COMBINED, "[the] eligible [Autorank] paths of %player%", "%player%'s eligible [Autorank] paths");
-            Skript.registerExpression(ExprCompletedPaths.class, String.class, ExpressionType.COMBINED, "[the] (completed|finished) [Autorank] paths of %player%", "%player%'s (completed|finished) [Autorank] paths");
-
-            //Events
-            Skript.registerEvent("Autorank Requirement Completion", EvtRequirementCompletion.class, RequirementCompleteEvent.class, "[Autorank] requirement complet(e|ion)");
-            EventValues.registerEventValue(RequirementCompleteEvent.class, Player.class, new Getter<Player, RequirementCompleteEvent>() {
-                public Player get(RequirementCompleteEvent e) {
-                    return e.getPlayer();
-                }
-            }, 0);
-            EventValues.registerEventValue(RequirementCompleteEvent.class, String.class, new Getter<String, RequirementCompleteEvent>() {
-                public String get(RequirementCompleteEvent e) {
-                    return e.getRequirement().getDescription();
-                }
-            }, 0);
-        }
+        //Events
+        Skript.registerEvent("Autorank Requirement Completion", SkUniversalEvent.class, RequirementCompleteEvent.class, "[Autorank] requirement complet(e|ion)");
+        EventValues.registerEventValue(RequirementCompleteEvent.class, Player.class, new Getter<Player, RequirementCompleteEvent>() {
+            public Player get(RequirementCompleteEvent e) {
+                return e.getPlayer();
+            }
+        }, 0);
+        EventValues.registerEventValue(RequirementCompleteEvent.class, String.class, new Getter<String, RequirementCompleteEvent>() {
+            public String get(RequirementCompleteEvent e) {
+                return e.getRequirement().getDescription();
+            }
+        }, 0);
     }
 }
--- src/us/_donut_/skuniversal/bedwars/BedwarsRegister.java
+++ src/us/_donut_/skuniversal/bedwars/BedwarsRegister.java
@@ -2,32 +2,27 @@ package us._donut_.skuniversal.bedwars;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.ExpressionType;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import us._donut_.skuniversal.SkUniversal;
 
 public class BedwarsRegister {
+
     public static void registerBedwars() {
-        if (Bukkit.getServer().getPluginManager().getPlugin("Bedwars") != null) {
-            SkUniversal.hookedPlugins.add("BedWars");
+        //Conditions
+        Skript.registerCondition(CondInGame.class, "%player% is [playing] in [a] Bedwars game");
+        Skript.registerCondition(CondSpectating.class, "%player% is spectating [a] Bedwars game");
+        Skript.registerCondition(CondGameExists.class, "[a] Bedwars game [(named|with name)] %string% exists");
+        Skript.registerCondition(CondGameRunning.class, "[the] Bedwars game [(named|with name)] %string% is (running|active)");
+        Skript.registerCondition(CondGameStartable.class, "[the] Bedwars game [(named|with name)] %string% is (startable|able to start)");
 
-            //Conditions
-            Skript.registerCondition(CondInGame.class, "%player% is [playing] in [a] Bedwars game");
-            Skript.registerCondition(CondSpectating.class, "%player% is spectating [a] Bedwars game");
-            Skript.registerCondition(CondGameExists.class, "[a] Bedwars game [(named|with name)] %string% exists");
-            Skript.registerCondition(CondGameRunning.class, "[the] Bedwars game [(named|with name)] %string% is (running|active)");
-            Skript.registerCondition(CondGameStartable.class, "[the] Bedwars game [(named|with name)] %string% is (startable|able to start)");
+        //Effects
+        Skript.registerEffect(EffStartGame.class, "start [the] Bedwars game [(named|with name)] %string%");
+        Skript.registerEffect(EffEndGame.class, "(end|stop) [the] Bedwars game [(named|with name)] %string%");
+        Skript.registerEffect(EffJoinGame.class, "make %player% join [the] Bedwars game [(named|with name)] %string%");
+        Skript.registerEffect(EffLeaveGame.class, "make %player% leave [the] Bedwars game [(named|with name)] %string%");
 
-            //Effects
-            Skript.registerEffect(EffStartGame.class, "start [the] Bedwars game [(named|with name)] %string%");
-            Skript.registerEffect(EffEndGame.class, "(end|stop) [the] Bedwars game [(named|with name)] %string%");
-            Skript.registerEffect(EffJoinGame.class, "make %player% join [the] Bedwars game [(named|with name)] %string%");
-            Skript.registerEffect(EffLeaveGame.class, "make %player% leave [the] Bedwars game [(named|with name)] %string%");
-
-            //Expressions
-            Skript.registerExpression(ExprGames.class, String.class, ExpressionType.SIMPLE, "[[the ]names of] [all] [the] [existing] Bedwars games");
-            Skript.registerExpression(ExprGameOfPlayer.class, String.class, ExpressionType.COMBINED, "[[the ]name of] [the] Bedwars game of %player%", "[[the ]name of] %player%'s Bedwars game");
-            Skript.registerExpression(ExprPlayers.class, Player.class, ExpressionType.SIMPLE, "[the] players in [the] Bedwars game [(named|with name)] %string%");
-        }
+        //Expressions
+        Skript.registerExpression(ExprGames.class, String.class, ExpressionType.SIMPLE, "[[the ]names of] [all] [the] [existing] Bedwars games");
+        Skript.registerExpression(ExprGameOfPlayer.class, String.class, ExpressionType.COMBINED, "[[the ]name of] [the] Bedwars game of %player%", "[[the ]name of] %player%'s Bedwars game");
+        Skript.registerExpression(ExprPlayers.class, Player.class, ExpressionType.SIMPLE, "[the] players in [the] Bedwars game [(named|with name)] %string%");
     }
 }
