402f81f9cccb4b6a595e72553c1bda1debfe4e36
Donut <donut42915@gmail.com>
2018-02-03 19:14:01 -0500

    Add files via upload

--- src/us/_donut_/skuniversal/lwc/LWCRegister.java
+++ src/us/_donut_/skuniversal/lwc/LWCRegister.java
@@ -1,15 +1,12 @@
 package us._donut_.skuniversal.lwc;
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.doc.Description;
-import ch.njol.skript.doc.Examples;
-import ch.njol.skript.doc.Name;
 import ch.njol.skript.lang.ExpressionType;
 import org.bukkit.OfflinePlayer;
 
 public class LWCRegister {
 
-    public static void registerLWC() {
+    public LWCRegister() {
         //Conditions
         Skript.registerCondition(CondProtected.class, "[the] %block% is (locked|protected) [by LWC]");
         Skript.registerCondition(CondAccess.class, "[the] %player% (has access to|can access) [LWC] [(locked|protected)]] [the] %block%");
@@ -19,7 +16,7 @@ public class LWCRegister {
         Skript.registerEffect(EffRemoveProtection.class, "(remove|delete) [the] [LWC] (protection|lock) from [the] %block%");
 
         //Expressions
-        Skript.registerExpression(ExprOwner.class, OfflinePlayer.class, ExpressionType.COMBINED, "[the] [LWC] owner of [the] %block%", "%block%'s [LWC] owner");
+        Skript.registerExpression(ExprLWCOwner.class, OfflinePlayer.class, ExpressionType.COMBINED, "[the] [LWC] owner of [the] %block%", "%block%'s [LWC] owner");
         Skript.registerExpression(ExprProtectionNumber.class, Number.class, ExpressionType.COMBINED, "[the] [(number|amount) of] [LWC] protections of %player%", "%player%'s [(number|amount) of] [LWC] protections");
     }
 }
