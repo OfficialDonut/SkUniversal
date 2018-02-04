5729e344370cbcd1e6a475c3c90a64f68c3a10ec
Donut <donut42915@gmail.com>
2018-02-03 19:14:42 -0500

    Add files via upload

--- src/us/_donut_/skuniversal/parties/PartiesRegister.java
+++ src/us/_donut_/skuniversal/parties/PartiesRegister.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 
 public class PartiesRegister {
 
-    public static void registerParties() {
+    public PartiesRegister() {
         //Conditions
         Skript.registerCondition(CondPartyExists.class, "party [(named|with name)] %string% exists");
         Skript.registerCondition(CondInParty.class, "%offlineplayer% is in [a] party");
@@ -23,7 +23,7 @@ public class PartiesRegister {
         Skript.registerExpression(ExprPartyOfPlayer.class,String.class, ExpressionType.COMBINED, "[the] party [name] of %offlineplayer%", "%offlineplayer%'s party [name]");
         Skript.registerExpression(ExprMembers.class, OfflinePlayer.class, ExpressionType.COMBINED, "[the] members of [the] party [(named|with name)] %string%", "[the] party [(named|with name)] %string%'s members");
         Skript.registerExpression(ExprOnlineMembers.class, Player.class, ExpressionType.COMBINED, "[the] online members of [the] party [(named|with name)] %string%", "[the] party [(named|with name)] %string%'s online members");
-        Skript.registerExpression(ExprKills.class, Number.class, ExpressionType.COMBINED, "[the] [(number|amount of)] kills of [the] party [(named|with name)] %string%", "[the] party [(named|with name)] %string%'s [(number|amount of)] kills");
+        Skript.registerExpression(ExprPartyKills.class, Number.class, ExpressionType.COMBINED, "[the] [(number|amount of)] kills of [the] party [(named|with name)] %string%", "[the] party [(named|with name)] %string%'s [(number|amount of)] kills");
         Skript.registerExpression(ExprHome.class, Location.class, ExpressionType.COMBINED, "[the] home [loc[ation]] of [the] party [(named|with name)] %string%", "[the] party [(named|with name)] %string%'s home [loc[ation]]");
     }
 }
