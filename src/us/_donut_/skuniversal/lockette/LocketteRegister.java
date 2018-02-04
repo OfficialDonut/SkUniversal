bade9a25d806073a77241c4849dad7e0a79b5d88
Donut <donut42915@gmail.com>
2018-02-03 19:13:20 -0500

    Add files via upload

--- src/us/_donut_/skuniversal/lockette/LocketteRegister.java
+++ src/us/_donut_/skuniversal/lockette/LocketteRegister.java
@@ -5,11 +5,11 @@ import ch.njol.skript.lang.ExpressionType;
 import org.bukkit.entity.Player;
 
 public class LocketteRegister {
-    public static void registerLockette() {
+    public LocketteRegister() {
         //Conditions
-        Skript.registerCondition(CondLocked.class, "[the] %block% is (locked|protected) [by Lockette]");
+        Skript.registerCondition(CondLockedByLockette.class, "[the] %block% is (locked|protected) [by Lockette]");
 
         //Expressions
-        Skript.registerExpression(ExprOwner.class, Player.class, ExpressionType.COMBINED, "[the] [Lockette] owner of [the] %block%", "[the] %block%'s [Lockette] owner");
+        Skript.registerExpression(ExprLocketteOwner.class, Player.class, ExpressionType.COMBINED, "[the] [Lockette] owner of [the] %block%", "[the] %block%'s [Lockette] owner");
     }
 }
