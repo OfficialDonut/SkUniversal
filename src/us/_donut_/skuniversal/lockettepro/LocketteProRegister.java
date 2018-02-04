673b3ff70922ed2f99c646f398ce8c6a7684a426
Donut <donut42915@gmail.com>
2018-02-03 19:13:40 -0500

    Add files via upload

--- src/us/_donut_/skuniversal/lockettepro/LocketteProRegister.java
+++ src/us/_donut_/skuniversal/lockettepro/LocketteProRegister.java
@@ -6,16 +6,16 @@ import org.bukkit.OfflinePlayer;
 
 public class LocketteProRegister {
 
-    public static void registerLockettePro() {
+    public LocketteProRegister() {
         //Conditions
         Skript.registerCondition(CondUser.class, "%player% is [a] [LockettePro] user of [the] %block%");
-        Skript.registerCondition(CondLocked.class, "[the] %block% is (locked|protected) [by LockettePro]");
+        Skript.registerCondition(CondLockedByLockettePro.class, "[the] %block% is (locked|protected) [by LockettePro]");
         Skript.registerCondition(CondLockable.class, "[the] %block% is (lockable|able to be locked) [by LockettePro]");
 
         //Effects
         Skript.registerEffect(EffLock.class, "(put|place) [a] [LockettePro] sign on [the] %string% [block]face of [the] %block% with %player% as [the] owner");
 
         //Expressions
-        Skript.registerExpression(ExprOwner.class, OfflinePlayer.class, ExpressionType.COMBINED, "[LockettePro] owner of [the] %block%", "[the] %block%'s [LockettePro] owner");
+        Skript.registerExpression(ExprLocketteProOwner.class, OfflinePlayer.class, ExpressionType.COMBINED, "[LockettePro] owner of [the] %block%", "[the] %block%'s [LockettePro] owner");
     }
 }
